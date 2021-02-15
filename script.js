dd0bb47fd7e78bc2ba1f58076c68d010d4c1f75e
Stefanie <stefaniefanie73@gmail.com>
2021-02-15 23:02:52 +0700

    update

--- script.js
+++ script.js
@@ -1,62 +1,82 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterButton = document.getElementById('twitter');
-const newQuoteButton = document.getElementById('new-quote');
+const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-function showLoadingSpinner(){
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-}
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+let initialLoad = true;
 
-function removeLoadingSpinner(){
-    if(!loader.hidden){
-        quoteContainer.hidden = false;
+//Unsplash API
+let count = 5;
+const apiKey = '77Wj16Ou2u-TsPNA5yN5cOutpolEqp2-q4aNq6NxGmA';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Check if all images were loaded
+function imageLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
         loader.hidden = true;
+        count = 30;
+        apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
     }
 }
 
-// Get Quote from API
-async function getQuote(){
-    showLoadingSpinner();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        // if author is blank, add 'Unknown'
-        if(data.quoteAuthor === ''){
-            authorText.innerText = 'Unknown';
-        }else {
-            authorText.innerText = data.quoteAuthor;
-        }
-        // Reduce font size for long quotes
-        if(data.quoteText.length > 120){
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote');
-        }
-        quoteText.innerText = data.quoteText;
-        //stop Loader, show quote
-        removeLoadingSpinner();
-    } catch (error) {
-       alert("Sorry, data not found"); 
+// Helper Function to set attributes on DOM Elements
+function setAttributes(element, attributes){
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key]);
     }
 }
 
-// Twitter Quote
-function tweetQuote() {
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl, '_blank');
+// Create Elements for links & photos, add to DOM
+function displayPhotos(){
+    totalImages = photosArray.length;
+    //Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        // Create <img> for photos
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        //Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+
+        //Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
 }
 
-// Event Listeners
-newQuoteButton.addEventListener('click', getQuote);
-twitterButton.addEventListener('click', tweetQuote);
+// Get photos from Unsplash API
+async function getPhotos(){
+    try{
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    }catch(error){
+        // CAtch error here
+    }
+}
 
+// Check to see if scrolling near bottom of photo, load more photos
+window.addEventListener('scroll', ()=>{
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        imagesLoaded = 0;
+        getPhotos();
+    }
+});
 
-// On Load
-getQuote();
+// On load
+getPhotos();
